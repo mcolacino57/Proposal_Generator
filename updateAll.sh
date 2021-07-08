5dd8a3330a3d175603259eb48db4bba74aaa1223
Michael COLACINO <mcolacino@mac.com>
2021-07-08 15:31:03 -0400

    Create updateAll.sh

---
+++ updateAll.sh
@@ -0,0 +1,19 @@
+cp formUtil.js ../Operating_Expenses
+cp formUtil.js ../Tenant_Improvements
+cp formUtil.js ../Proposal_Start
+cp formUtil.js ../Proposal_Overview
+cp formUtil.js ../Apples
+
+cp classes.js ../Operating_Expenses
+cp classes.js ../Tenant_Improvements
+cp classes.js ../Proposal_Start
+cp classes.js ../Proposal_Overview
+cp classes.js ../Apples
+
+cp gcloudSQL.js ../Operating_Expenses
+cp gcloudSQL.js ../Tenant_Improvements
+cp gcloudSQL.js ../Proposal_Start
+cp gcloudSQL.js ../Proposal_Overview
+cp gcloudSQL.js ../Apples
+
+
